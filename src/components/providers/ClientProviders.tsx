944c126d34839846afa33e7ed1ee7ed24eb15f4a
Fadilah Arifki <fadilah.arifki29@gmail.com>
2026-08-10 11:16:29 +0700

    pwa

--- src/components/providers/ClientProviders.tsx
+++ src/components/providers/ClientProviders.tsx
@@ -4,11 +4,15 @@ import React from 'react';
 import { QueryClientProvider } from '@tanstack/react-query';
 import { queryClient } from '@/lib/queryClient';
 import { MainLayoutTemplate } from '@/components/templates/MainLayoutTemplate';
+import { PwaInstallPrompt } from '@/components/molecules/PwaInstallPrompt';
 
 export function ClientProviders({ children }: { children: React.ReactNode }) {
   return (
     <QueryClientProvider client={queryClient}>
-      <MainLayoutTemplate>{children}</MainLayoutTemplate>
+      <MainLayoutTemplate>
+        {children}
+        <PwaInstallPrompt />
+      </MainLayoutTemplate>
     </QueryClientProvider>
   );
 }
